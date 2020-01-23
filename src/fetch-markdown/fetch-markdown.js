0b75a795d0ea881de073161f9b6e894e779ad828
Stephen Esser <Stephen.Esser@Cerner.com>
2020-01-22 18:27:32 -0600

    Cleanup and document markdown code generation

--- src/fetch-markdown/fetch-markdown.js
+++ src/fetch-markdown/fetch-markdown.js
@@ -39,9 +39,7 @@ class Markdown {
 
     // Iterate the individual project changelogs.
     for (let index = 0; index < repos.length; index += 1) {
-      const repo = repos[index];
-
-      changelogs.push({ repo, component: repo, url: Markdown.changelogURL(repo) });
+      changelogs.push({ repo: repos[index], url: Markdown.changelogURL(repos[index]) });
     }
 
     const promises = changelogs.map(({ repo, component, url }) => (
@@ -70,40 +68,48 @@ class Markdown {
     return Markdown.changelogs().then(Markdown.format);
   }
 
+  /**
+   * Splices an array of changelog into formatted markdown files sorted by date.
+   * @param {array<Object>} changelogs - An array of changelogs.
+   * @returns {array} - An array of markdown files sorted by date.
+   */
   static format(changelogs) {
     const releases = {};
 
     for (let index = 0; index < changelogs.length; index += 1) {
-      const { component, changelog } = changelogs[index];
+      const { repo, component, changelog } = changelogs[index];
 
       const lines = changelog.split('\n');
-      let currentLine = lines.findIndex((line) => RELEASE_REGEX.test(line));
 
       let currentDate;
+      let currentLine = 0;
 
-      if (currentLine >= 0) {
-        while (currentLine < lines.length) {
-          const line = lines[currentLine];
+      while (currentLine < lines.length) {
+        const line = lines[currentLine];
 
-          if (RELEASE_REGEX.test(line)) {
-            const version = line.split(' -')[0];
-            currentDate = line.substring(line.indexOf('(') + 1, line.indexOf(')'));
+        if (RELEASE_REGEX.test(line)) {
+          const version = line.split(' -')[0];
 
-            if (releases[currentDate] === undefined) {
-              releases[currentDate] = `# ${currentDate}\n`;
-            }
+          currentDate = line.substring(line.indexOf('(') + 1, line.indexOf(')'));
 
-            releases[currentDate] += `## ${component} (${version})\n`;
-          } else if (line.indexOf('----') < 0) {
-            releases[currentDate] += `${line}\n`;
+          // Create a release date entry if one does not already exist.
+          if (releases[currentDate] === undefined) {
+            releases[currentDate] = `# ${currentDate}\n`;
           }
 
-          currentLine += 1;
+          // Append the component release information.
+          releases[currentDate] += `## ${component || repo} (${version})\n`;
+          currentLine += 1; // Release entries are followed by a horizontal rule. Skip it.
+        } else if (currentDate) {
+          releases[currentDate] += `${line}\n`;
         }
+
+        currentLine += 1;
       }
     }
 
-    return Object.keys(releases).sort((a, b) => new Date(b) - new Date(a)).map((date) => marked(releases[date]));
+    // Sort the releases by date and compile them into renderable html markdown.
+    return Object.keys(releases).sort((a, b) => (new Date(b) - new Date(a))).map((date) => marked(releases[date]));
   }
 }
 
